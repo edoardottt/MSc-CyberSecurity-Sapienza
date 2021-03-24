9909278c65cd7a724045060a397d99408446bf58
edoardottt <edoardott@gmail.com>
2021-03-24 17:03:09 +0100

    update

---
+++ Internet-of-Things/lab/exercises/ex008/ex008.cpp
@@ -0,0 +1,16 @@
+/*
+Write a program that promts the user to input the sentence
+"This is my first sentence.", and prints that sentence.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string mystr;
+    cout << "Enter a sentence: ";
+    getline(cin, mystr);
+    cout << "You entered " << mystr << " \n";
+}

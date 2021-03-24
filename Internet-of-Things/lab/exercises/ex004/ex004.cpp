907ca71e73bc8a35eb54255f8068a76d539c96a3
edoardottt <edoardott@gmail.com>
2021-03-24 16:48:42 +0100

    update

---
+++ Internet-of-Things/lab/exercises/ex004/ex004.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 0;
+    cout << "Please enter an integer value: ";
+    cin >> i;
+    cout << "The value you entered is " << i;
+    cout << " and its double is " << i * 2 << ".\n ";
+    return 0;
+}

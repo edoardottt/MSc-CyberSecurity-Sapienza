baba5206efcf0a500c2369fda647fc3a8a2f3805
gilfoyle97 <edoardott@gmail.com>
2021-04-27 12:42:23 +0200

    Add files via upload

---
+++ Practical-Network-Defense/tests/lab3/ex2/s1/var/www/html/formget.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html> 
+

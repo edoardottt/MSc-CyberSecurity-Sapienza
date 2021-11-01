f762239ac7d60d0c9eb68a73a9bdda1d06b4cf7b
gilfoyle97 <edoardott@gmail.com>
2021-11-01 09:52:25 +0100

    Add files via upload

---
+++ Statistics/lesson6/sources/Lesson6/Lesson6/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Lesson6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(99, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(321, 26);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(476, 26);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "M (number of paths)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "N (number of points)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(321, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "P (Probabilty of success)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(476, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "e (epsilon)";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1184, 648);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(819, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(378, 54);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1226, 766);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Lesson6";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Statistics/lesson6/sources/Lesson6/Lesson6/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // == GLOBAL VARIABLES ==
+        int m = 0; //number of paths
+        int n = 0; //number of points per path
+        double p = 0; //probability of success (0 - 1)
+        double epsilon = 0; //epsilon
+        Point point; // mouse location handler
+        double minX_Window = 0;
+        double maxX_Window = 0;
+        double minY_Window = 0;
+        double maxY_Window = 0;
+        double rangeX = 0;
+        double rangeY = 0;
+        Dictionary<int, List<double>> points; // All the random generated points
+        Bitmap b;
+        Graphics g;
+        Font smallFont = new Font("Calibri", 13, FontStyle.Regular, GraphicsUnit.Pixel);
+        Rectangle viewport;
+
+        // == INITIALIZE THE GRAPHICS OBJECT ==
+        private void initGraphics()
+        {
+            viewport = new Rectangle(10, 10, Convert.ToInt32(pictureBox1.Width / 2) - 50, pictureBox1.Height - 100);
+            b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(b);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+        }
+
+        // == CALCULATE THE X VALUE IN THE VIEWPORT ==
+        private int calculateXViewport(double x_world, Rectangle viewport, double minX, double rangeX)
+        {
+            // X = L + ((x - minX)/Rx)W
+            return Convert.ToInt32(viewport.Left + ((x_world - minX) / rangeX) * viewport.Width);
+        }
+
+        // == CALCULATE THE Y VALUE IN THE VIEWPORT ==
+        private int calculateYViewport(double y_world, Rectangle viewport, double minY, double rangeY)
+        {
+            // Y = T + H - (((y - minY)/Ry)H)
+            return Convert.ToInt32(viewport.Top + viewport.Height - ((((y_world - minY) / rangeY)) * viewport.Height));
+        }
+
+        // == CHECK IF USER INPUT IS VALID ==
+        private bool checkInput()
+        {
+            m = 0;
+            if (!Int32.TryParse(textBox1.Text, out m))
+            {
+                richTextBox1.Text = "M must be a valid integer!";
+                return false;
+            }
+            n = 0;
+            if (!Int32.TryParse(textBox2.Text, out n))
+            {
+                richTextBox1.Text = "N must be a valid integer!";
+                return false;
+            }
+            p = 0;
+            if (!Double.TryParse(textBox3.Text, out p))
+            {
+                richTextBox1.Text = "P must be a valid double!";
+                return false;
+            }
+            if (p < 0 || p > 1)
+            {
+                richTextBox1.Text = "P must be a valid double between 0 and 1!";
+                return false;
+            }
+            epsilon = 0;
+            if (!Double.TryParse(textBox4.Text, out epsilon))
+            {
+                richTextBox1.Text = "epsilon must be a valid double!";
+                return false;
+            }
+            if (epsilon < 0 || epsilon > 1)
+            {
+                richTextBox1.Text = "Epsilon must be a valid double between 0 and 1!";
+                return false;
+            }
+            return true;
+        }
+
+        // == PLOT EVERYTHING ==
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool inputOk = checkInput();
+            if (inputOk)
+            {
+                richTextBox1.Text = "> " + m + " " + n + " " + p + " " + epsilon + "\n";
+            }
+        }
+    }
+}
